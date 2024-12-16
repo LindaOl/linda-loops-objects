216c7ea621afdf2eb0e9cdacddd76e5cc41a87ac
LindaOl <183224471+lillesh@users.noreply.github.com>
2024-12-16 17:47:44 +0100

    Finished the exercises.

--- src/index.js
+++ src/index.js
@@ -22,9 +22,8 @@ const pokemonList = () => {
   for (i = 0; i < pokemons.length; i++) {
     console.log(pokemonList[i]);
   }
-}
-
-pokemonList()
+};
+pokemonList();
 
 
 
@@ -32,20 +31,34 @@ pokemonList()
 // Create an object for the pokemon Pikachu.
 // it could have properties such as name, element,
 // strength, color, amountOfLegs or something like that.
+let pokemon = {
+  name: 'Pikachu',
+  gender: 'male',
+  element: 'electric',
+  color: 'yellow',
+  combatPower: 1527
+};
 
 // 3)
 // console.log something like
 // 'Pikachu is a electricity pokemon with the strength of 32'
 // Selecting some of the information about your pikachu object.
+console.log(`This ${pokemon.gender} ${pokemon.name} is an ${pokemon.element} type pokemon with the CP of ${pokemon.combatPower}.`);
 
 // 4)
 // Add the property stillToCatch: true to the Pikachu object.
+pokemon.stillToCatch = true;
+
 
 // 5)
 // change the value of the key strength in the Pikachu object.
+pokemon.combatPower = 2035;
+
 
 // 6)
 // Delete a property from the Pikachu object.
+delete pokemon.gender;
+console.log(pokemon);
 
 // 7)
 // Fill up this array with a few more pokemons.
@@ -56,19 +69,54 @@ const moreAboutPokemons = [
   {
     name: "Bulbasaur",
     element: "water",
-    strength: 13
+    combatPower: 1527
   },
   {
     name: "Charmander",
     element: "fire",
-    strength: 32
+    combatPower: 923
+  },
+  {
+    name: "Hitmonchan",
+    element: "fighting",
+    combatPower: 2314
+  },
+  {
+    name: "Woobat",
+    element: ["psychic", "flying"],
+    combatPower: 124
+  },
+  {
+    name: "Eevee",
+    element: "normal",
+    combatPower: 1079
+  },
+  {
+    name: "Abra",
+    element: "psychic",
+    combatPower: 2358
   }
 ];
 
 // 8)
 // Print out all the NAMES of the pokemons in this new array.
 
+const newPokemonList = () => {
+  moreAboutPokemons.forEach((pokemon) => {
+    console.log(`${pokemon.name}`)
+  })
+};
+newPokemonList();
+
+
 // 9)
 // Print out something like
 // 'Bulbasaur is a water pokemon with the strength of 32'
 // Do this for all the pokemons.
+
+const printInformation = () => {
+  moreAboutPokemons.forEach((pokemon) => {
+    console.log(`${pokemon.name} is a ${pokemon.element} pokemon with the CP of ${pokemon.combatPower}`)
+  })
+};
+printInformation();
